596a381297fece16a2b18448b808e03960902c31
Abdul Motin <motin.mmk.jr@gmail.com>
2021-05-19 15:00:44 +0600

    first commit

---
+++ app/views/partials/product/edit.php
@@ -0,0 +1,112 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "edit-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$data = $this->view_data;
+//$rec_id = $data['__tableprimarykey'];
+$page_id = $this->route->page_id;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="edit"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title">Edit  Product</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form novalidate  id="" role="form" enctype="multipart/form-data"  class="form page-form form-horizontal needs-validation" action="<?php print_link("product/edit/$page_id/?csrf_token=$csrf_token"); ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="product_name">Product Name <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-product_name"  value="<?php  echo $data['product_name']; ?>" type="text" placeholder="Enter Product Name"  required="" name="product_name"  class="form-control " />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="product_desc">Product Desc <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <textarea placeholder="Enter Product Desc" id="ctrl-product_desc"  required="" rows="5" name="product_desc" class=" form-control"><?php  echo $data['product_desc']; ?></textarea>
+                                                    <!--<div class="invalid-feedback animated bounceIn text-center">Please enter text</div>-->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="product_stock_lavel">Product Stock Lavel <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-product_stock_lavel"  value="<?php  echo $data['product_stock_lavel']; ?>" type="number" placeholder="Enter Product Stock Lavel" step="1"  required="" name="product_stock_lavel"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="product_buy_price">Product Buy Price <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-product_buy_price"  value="<?php  echo $data['product_buy_price']; ?>" type="number" placeholder="Enter Product Buy Price" step="0.1"  required="" name="product_buy_price"  class="form-control " />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="product_sel_price">Product Sel Price <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="">
+                                                            <input id="ctrl-product_sel_price"  value="<?php  echo $data['product_sel_price']; ?>" type="number" placeholder="Enter Product Sel Price" step="0.1"  required="" name="product_sel_price"  class="form-control " />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-ajax-status"></div>
+                                            <div class="form-group text-center">
+                                                <button class="btn btn-primary" type="submit">
+                                                    Update
+                                                    <i class="fa fa-send"></i>
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
